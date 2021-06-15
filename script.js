3cf321931c235fd81e7d3f5e8bf9dee0b2ce1385
Stephanie041996 <michellesakuhuni5@gmail.com>
2021-06-15 10:39:15 +0200

    Add table to display books

--- script.js
+++ script.js
@@ -3,19 +3,37 @@ let library = [{name:"Digital Fortress", author:"Dan Brown"},{name:"Improbable",
 function Book(name, author){
   this.name =  name;
   this.author = author;
+  this.pages = pages;
+  this.status = status;
 }
 
 function addLibrary(){
   let book_name = document.getElementById("newBookName").value;
   let book_author = document.getElementById("newBookAuthor").value;
-  let book = new Book(book_name,book_author)
+  let pages_read =  parseInt(document.getElementById("pages").value);
+  let read_status = document.getElementById("status").value;
+  let book = new Book(book_name,book_author,pages_read,read_status);
   library.push(book)
+  console.log
 }
 //addLibrary()
 
-// for(let i=0; i<library.length; i++){
-//  console.table(library[i])
+for(let i=0; i<library.length; i++){
+ console.table(library[i])
   
-// }
+}
 
-console.table(library)
+//Insert data function
+function Insert_Data() {
+  var table = document.getElementById("datas");
+  table.innerHTML="";
+  var tr="";
+  library.forEach(x=>{
+     tr+='<tr>';
+     tr+='<td>'+x.name+'</td>'+'<td>'+x.author+'</td>'+'<td>'+x.pages+'</td>'+'<td>'+x.status+'</td>'
+     tr+='</tr>'
+
+  })
+  table.innerHTML+=tr;
+  //Help......  
+}
